14f12d13770eacbfe0af7259990c54f687f9a2e4
AustinWithoutPowers <austinrudkin@live.com>
2024-12-04 18:28:18 +1300

    Meant to publish all of this

--- generics.py
+++ generics.py
@@ -20,22 +20,3 @@ def merge_sort(unmerged_list):
                 sorted_list += [right_half.pop(0)]
         
         return sorted_list
-
-f = open("../Inputs/day1_input.txt", "r")
-double_array = f.readlines()
-f.close()
-
-left_list, right_list = [], []
-
-for row in double_array:
-    left_list += [row.strip().split()[0]]
-    right_list += [row.strip().split()[1]]
-
-sorted_left_list = merge_sort(left_list)
-sorted_right_list = merge_sort(right_list)
-
-total_sum = 0
-for i in range(len(sorted_left_list)):
-    total_sum += abs(int(sorted_left_list[i]) - int(sorted_right_list[i]))
-
-print(total_sum)
